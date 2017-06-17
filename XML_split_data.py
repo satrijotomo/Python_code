abdbc594c60336b65444368280b4b89419334a88
Bambang Satrijotomo <bambang.satrijotomo@gmail.com>
2017-06-17 15:37:44 -0500

    Add files via upload

---
+++ XML_split_data.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# So, the problem is that the gigantic file is actually not a valid XML, because
+# it has several root elements, and XML declarations.
+# It is, a matter of fact, a collection of a lot of concatenated XML documents.
+# So, one solution would be to split the file into separate documents,
+# so that you can process the resulting files as valid XML documents.
+
+import xml.etree.ElementTree as ET
+PATENTS = 'patent.data'
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def split_file(filename):
+    """
+    Split the input file into separate files, each containing a single patent.
+    As a hint - each patent declaration starts with the same line that was
+    causing the error found in the previous exercises.
+    
+    The new files should be saved with filename in the following format:
+    "{}-{}".format(filename, n) where n is a counter, starting from 0.
+    """
+    with open(filename, 'r') as f:
+    #Opens the file given and is assigned to the variable f
+        prolog = f.readline()
+        print prolog ## returns: <?xml version="1.0" encoding="UTF-8"?>
+        l1 = f.read().split(prolog) #Assigns to variable prolog the the first line in the variable f
+        for n,line in enumerate(l1): # n = 4 (blocks), line is the rows inside each block
+            with open('{}-{}'.format(filename, n), 'w') as write:
+                write.writelines(prolog) # Add prolog to the beginning of the file
+                write.write(line.strip()) # Write the rest of the lines
+                
+
+    pass
+
+
+def test():
+    split_file(PATENTS)
+    for n in range(4):
+        try:
+            fname = "{}-{}".format(PATENTS, n)
+            f = open(fname, "r")
+            if not f.readline().startswith("<?xml"):
+                print "You have not split the file {} in the correct boundary!".format(fname)
+            f.close()
+        except:
+            print "Could not find file {}. Check if the filename is correct!".format(fname)
+
+
+test()
