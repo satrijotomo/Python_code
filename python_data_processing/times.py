dd42b850b8d1bdc6dd487a7ca7f31a921b0a4763
satrijotomo <bambang.satrijotomo@gmail.com>
2017-12-25 18:41:34 -0600

    src: Complete times.py

--- python_data_processing/times.py
+++ python_data_processing/times.py
@@ -3,11 +3,13 @@ from dateutil.parser import parse
 
 def extract_time(mytime):
     parsedtime = parse(mytime)
-    return parsedtime
+    return parsedtime.hour
 
 
 df = pd.read_csv("hn_stories.csv", names = ["submission_time", "upvotes", "url", "headline"])
 allsubtime = df['submission_time']
-for row in allsubtime:
-    print(extract_time(row))
+
+timehour = allsubtime.apply(extract_time)
+timecount = timehour.value_counts(sort=True, ascending=False)
+print(timecount) 
 
